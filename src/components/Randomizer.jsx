ea3f45631a2d33f62de346172de3f46c89d5a5f4
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-12-27 21:40:47 -0500

    feat: implement Randomizer component with challenge fetching and shuffling functionality; add classnames for conditional styling for desktop view

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,17 +1,62 @@
 import  Nav from "./Navigation.jsx"
 import '../styles/Randomizer.css'
+import { useState, useEffect } from "react"
+import { getActiveChallenges, getRandomChallenge } from "../services/challengeService.js"
+import classNames from "classnames"
 
 export default function Randomizer(){
+    const [challenges, setChallenges] = useState([])
+    const [currentChallenge, setCurrentChallenge] = useState(null)
+
+    useEffect(() => {
+        async function loadChallenges() {
+            try {
+                const data = await getActiveChallenges()
+                setChallenges(data)
+            } catch (err) {
+                throw new Error(err)
+            } 
+        }
+        loadChallenges()
+    }, [])
+
+    function handleShuffle() {
+        const randomChallenge = getRandomChallenge(challenges)
+        setCurrentChallenge(randomChallenge)
+    }
+
     return (
         <>
         <div className="container">
             <Nav/>
             <section className="playground">
                 <h2>Pick Your Challenge</h2>
-                <button>Shuffle Project</button>
-                <div className="display-screen">
-                    <img src="src/assets/bulb.svg" />
-                    <p>Click shuffle to get a random project Challenge</p>
+                <button onClick={handleShuffle}>Shuffle Project</button>
+                <div className={classNames("display-screen", {
+                    "display-screen--active": currentChallenge,
+                    "display-screen--empty": !currentChallenge
+                })}>
+                    {
+                        currentChallenge ? (
+                            <>
+                                <p className="challenge-category">
+                                    {currentChallenge.category}
+                                </p>
+                                <h2 className="challenge-technology">
+                                    {currentChallenge.technology}
+                                </h2>
+                                <p className="challenge-platform">
+                                    {currentChallenge.platform}
+                                </p>
+                            </>
+                        ): (
+                            <>
+                                <img src="src/assets/bulb.svg" />
+                                <p>Click shuffle to get a random project Challenge</p>
+                            </>
+                        )
+                    }
+                    
                 </div>
             </section>
         </div>
