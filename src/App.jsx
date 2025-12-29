b6a1b958c523756e000c371f424f5dfa1a013ec9
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-12-29 01:57:28 -0500

    Update to manage state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './styles/App.css'
+import Home from './components/Home.jsx'
 import  Randomizer  from './components/Randomizer.jsx'
+import LeetCodeTracker from './components/LeetCodeTracker.jsx'
+import Portfolio from './components/Portfolio.jsx'
+import Movies from './components/Movies.jsx'
+import Nav from './components/Navigation.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState('home')
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home': 
+        return <Home />
+      case 'randomize':
+        return <Randomizer />
+      case 'lt-tracker':
+        return <LeetCodeTracker />
+      case 'portfolio':
+        return <Portfolio />
+      case 'movies':
+        return <Movies />
+      default:
+        return <Home />
+    }
+  }
 
   return (
     <>
-      <Randomizer />
+      <Nav currentPage={currentPage} onPageChange={setCurrentPage} />
+      <main className='main-content'>
+        {renderPage()}
+      </main>
     </>
   )
 }
