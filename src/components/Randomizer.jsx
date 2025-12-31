2fb7695490e00c31938ebf328f63cf7fbc7357a7
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2025-12-31 01:25:36 -0500

    Refactoring for modular css to avoid leaking

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,5 +1,4 @@
-import Nav from "./Navigation.jsx"
-import '../styles/Randomizer.css'
+import styles from '../styles/Randomizer.module.css'
 import { useState, useEffect } from "react"
 import { getActiveChallenges, getRandomChallenge } from "../services/challengeService.js"
 import classNames from "classnames"
@@ -27,36 +26,34 @@ export default function Randomizer() {
     }
 
     return (
-        <>
-            <section className="playground">
-                <h2>Pick Your Challenge</h2>
-                <button onClick={handleShuffle}>Shuffle Project</button>
-                <div className={classNames("display-screen", {
-                    "display-screen--active": currentChallenge,
-                    "display-screen--empty": !currentChallenge
-                })}>
-                    {
-                        currentChallenge ? (
-                            <>
-                                <p className="challenge-category">
-                                    {currentChallenge.category}
-                                </p>
-                                <h2 className="challenge-technology">
-                                    {currentChallenge.technology}
-                                </h2>
-                                <p className="challenge-platform">
-                                    {currentChallenge.platform}
-                                </p>
-                            </>
-                        ) : (
-                            <>
-                                <img src={bulbIcon} alt="bulb icon" />
-                                <p>Click shuffle to get a random project Challenge</p>
-                            </>
-                        )
-                    }
-                </div>
-            </section>
-        </>
+        <section className={styles.playground}>
+            <h2>Pick Your Challenge</h2>
+            <button onClick={handleShuffle}>Shuffle Project</button>
+            <div className={classNames(styles.displayScreen, {
+                [styles.displayScreenActive]: currentChallenge,
+                [styles.displayScreenEmpty]: !currentChallenge
+            })}>
+                {
+                    currentChallenge ? (
+                        <>
+                            <p className="challenge-category">
+                                {currentChallenge.category}
+                            </p>
+                            <h2 className="challenge-technology">
+                                {currentChallenge.technology}
+                            </h2>
+                            <p className="challenge-platform">
+                                {currentChallenge.platform}
+                            </p>
+                        </>
+                    ) : (
+                        <>
+                            <img src={bulbIcon} alt="bulb icon" />
+                            <p>Click shuffle to get a random project Challenge</p>
+                        </>
+                    )
+                }
+            </div>
+        </section>
     )
 }
