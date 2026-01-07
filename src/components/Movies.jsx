d2b079e3c24c062de178101e44a2c7af7451911f
Eliyahu Kainyah Yalley <46997419+Elikyals@users.noreply.github.com>
2026-01-07 00:46:52 -0500

    feat: Implemented start page of Movie section

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,12 +1,25 @@
 import styles from '../styles/Movies.module.css'
+import MovieIcon from "../assets/movie.svg?react"
 
 export default function Movies() {
     return (
-        <section className={styles.playground}>
-            <div className={styles.header}>
+        <>
+            <header className={styles.header}>
                 <h2>Find the film</h2>
                 <a>My Watchlist</a>
-            </div>
-        </section>
+            </header>
+            <form id="search-form" name="search-form">
+                <div className={styles.search}>
+                    <span className={`${styles['search-icon']} material-symbols-outlined`}>search</span>
+                    <input id="search-box" className={styles['search-input']} type="search" name="search" placeholder="Search for a movie" />
+                    <div className={styles.vline}></div>
+                    <input className={styles['search-btn']} type="submit" value="Search" />
+                </div>
+            </form>
+            <section className={styles.playground}>
+                <MovieIcon className={styles.icon} />
+                <h3>Start exploring</h3>
+            </section>
+        </>
     )
 }
